a3a97cea06dbaa97ca4ecf1b9cd98b353b498396
kemboi <evanskemb@gmail.com>
2023-05-19 17:03:40 +0300

    access modifiers added

--- src/Inheritance/Polymorphism/Shapes.java
+++ src/Inheritance/Polymorphism/Shapes.java
@@ -1,8 +1,20 @@
 package Inheritance.Polymorphism;
 
 public class Shapes {
+    int l;
+    int w;
+    private static final double PIE = 3.14;
+public double getPie(){
+    return PIE;
+}
     Shapes(){
     }
+
+    public Shapes(int l, int w) {
+        this.l = l;
+        this.w = w;
+    }
+
     void area(){
         System.out.println("Area of a shape");
     }
--- src/Inheritance/Polymorphism/Square.java
+++ src/Inheritance/Polymorphism/Square.java
@@ -1,8 +1,12 @@
 package Inheritance.Polymorphism;
 
 public class Square extends Shapes{
+    public Square(int l, int w) {
+        super(l, w);
+    }
+
     @Override
     void area() {
-        System.out.println("Area of a Square is l * l");
+        System.out.println("Area of a Square is l * l : "+ super.l * super.w);
     }
 }
