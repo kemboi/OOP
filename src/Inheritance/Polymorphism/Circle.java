00212a7cad013791e6e7d822e48dadf661dd8f96
kemboi <evanskemb@gmail.com>
2023-05-19 17:56:00 +0300

    added access modifier

--- src/Inheritance/Polymorphism/Circle.java
+++ src/Inheritance/Polymorphism/Circle.java
@@ -1,8 +1,13 @@
 package Inheritance.Polymorphism;
 
 public class Circle extends Shapes{
+    int r;
+    public Circle(int r) {
+        this.r = r;
+    }
+
     @Override
     void area() {
-        System.out.println("Area of a circle is Pie r * r");
+        System.out.println("Area of a circle is Pie r * r: "+ this.r * this.getPie() * this.r);
     }
 }
